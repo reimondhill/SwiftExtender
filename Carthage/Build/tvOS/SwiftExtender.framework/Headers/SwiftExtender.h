385ee7c39941e48603abe46393efd3acabfc994f
Ramon Haro Marques <ramonharomarques@gmail.com>
2020-05-20 18:46:13 +0100

    allowing carthage

---
+++ Carthage/Build/tvOS/SwiftExtender.framework/Headers/SwiftExtender.h
@@ -0,0 +1,18 @@
+//
+//  SwiftExtender.h
+//  SwiftExtender
+//
+//  Created by Ramon Haro Marques on 19/05/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftExtender.
+FOUNDATION_EXPORT double SwiftExtenderVersionNumber;
+
+//! Project version string for SwiftExtender.
+FOUNDATION_EXPORT const unsigned char SwiftExtenderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftExtender/PublicHeader.h>
+
+
